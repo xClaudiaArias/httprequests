af14f0da9ef82a2fdad2bb312e7a076475bd5924
xClaudiaArias <xClaudiaArias@gmail.com>
2024-11-19 18:04:49 -0500

    home cards styled

--- request-app/src/components/CountryCard.js
+++ request-app/src/components/CountryCard.js
@@ -3,12 +3,11 @@ import { Link } from 'react-router-dom'
 
 const CountryCard = ({country}) => {
     return (
-        <Link to={`/country/${country.cca3}`}>
+        <Link to={`/country/${country.cca3}`} className='card'>
             <div className="country-card">
                 <img src={country.flags.png} alt={country.flags.alt}></img>
                 <h3>{country.name.common}</h3>
                 <h4>{country.region} <span>({country.subregion})</span></h4>
-                {/* TODO: structure the country card with country.common.name, country.flag, etc.... */}
             </div>
         </Link>
     )
